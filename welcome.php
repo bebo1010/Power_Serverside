b64a681be21eee3f96c4ec2ab6581ef9861e52ae
bebo1010 <nick900828@gmail.com>
2023-06-02 14:33:23 +0800

    Rename start.php to welcome.php

--- welcome.php
+++ welcome.php
@@ -17,7 +17,6 @@
         <div class="tab">
             <button class="tablinks" onclick="loadTab(event, 'Search')" id="defaultOpen">查詢</button>
             <button class="tablinks" onclick="loadTab(event, 'Insert')">新增</button>
-            <button class="tablinks" onclick="loadTab(event, 'Delete')">刪除</button>
         </div>
 
         <div id="Search" class="tabcontent">
@@ -30,11 +29,11 @@
                     結束時間：<input type="date" id="結束時間" name="End_Date" value="<?php echo date('Y-m-d'); ?>">
                 </label>
 
-                <select id="商業/住宅" name="Diff_no">
-                    <option value="2">
+                <select id="商業/住宅" name="Environment">
+                    <option value="住宅">
                         住宅
                     </option>
-                    <option value="1">
+                    <option value="商用">
                         商用
                     </option>
                 </select>
@@ -47,10 +46,6 @@
             <!-- <form action="https://httpbin.org/post" method="post"> -->
             <form action="insert.php" method="post">
                 <label>
-                    使用者ID：<input type="number" id="使用者ID" name="UserID">
-                </label>
-
-                <label>
                     日期：<input type="date" id="日期輸入" name="Date" value="<?php echo date('Y-m-d'); ?>">
                 </label>
 
@@ -58,11 +53,11 @@
                     電表度數：<input type="number" id="電表度數輸入" name="KWH">
                 </label>
 
-                <select id="商業/住宅" name="Diff_no">
-                    <option value="2">
+                <select id="商業/住宅" name="Environment">
+                    <option value="住宅">
                         住宅
                     </option>
-                    <option value="1">
+                    <option value="商用">
                         商用
                     </option>
                 </select>
@@ -72,24 +67,23 @@
             </form>
         </div>
 
-        <div id="Delete" class="tabcontent">
-
-        </div>
-        
         <div id="Result">
             <table id="電表清單">
                 <tr id="標題">
                     <th style="width:5%">
                         ID
                     </th>
-                    <th style="width:30%">
+                    <th style="width:25%">
                         日期
                     </th>
-                    <th style="width:20%">
+                    <th style="width:15%">
                         電表度數
                     </th>
-                    <th style="width:5%">
-                        Diff_no
+                    <th style="width:10%">
+                        登記環境
+                    </th>
+                    <th style="width:15%" colspan="4">
+                        操作
                     </th>
                 </tr>
             </table>
@@ -99,5 +93,6 @@
             // Get the element with id="defaultOpen" and click on it
             document.getElementById("defaultOpen").click();
         </script>
+
     </body>
 </html>
