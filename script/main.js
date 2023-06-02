4bc8b94dd89eb327e3c4799145ece017c0df1d38
bebo1010 <nick900828@gmail.com>
2023-06-02 14:34:52 +0800

    Add half of modify functionality

--- script/main.js
+++ script/main.js
@@ -1,17 +1,193 @@
-function Add_Row(){
-    date = document.getElementById("日期輸入").value
-    actual_location = document.getElementById("地點輸入").value
-    power = document.getElementById("電表度數輸入").value
-
+function Add_Row(ID, Date, Power, Environment){
     new_row = document.getElementById("電表清單").insertRow()
+    new_row.id = "Row_" + ID
     new_row.classList.add("資料")
 
+    // Data
     cell = new_row.insertCell()
-    cell.innerHTML = date
+    cell.innerHTML = ID
     cell = new_row.insertCell()
-    cell.innerHTML = actual_location
+    cell.innerHTML = Date
     cell = new_row.insertCell()
-    cell.innerHTML = power
+    cell.innerHTML = Power
+    cell = new_row.insertCell()
+    cell.innerHTML = Environment
+
+    // Control
+    cell = new_row.insertCell()
+    modify_button = document.createElement("button")
+    cell.appendChild(modify_button)
+    modify_button.id = "Modify_" + ID
+    modify_button.innerHTML = "修改"
+    modify_button.addEventListener("click", function(){
+        controller(this.id)
+    })
+
+    cell = new_row.insertCell()
+    delete_button = document.createElement("button")
+    cell.appendChild(delete_button)
+    delete_button.id = "Delete_" + ID
+    delete_button.innerHTML = "刪除"
+    delete_button.addEventListener("click", function(){
+        controller(this.id)
+    })
+
+    cell = new_row.insertCell()
+    confirm_button = document.createElement("button")
+    cell.appendChild(confirm_button)
+    confirm_button.id = "Confirm_" + ID
+    confirm_button.innerHTML = "確認"
+    confirm_button.style.display = "none"
+    confirm_button.addEventListener("click", function(){
+        controller(this.id)
+    })
+
+    cell = new_row.insertCell()
+    cancel_button = document.createElement("button")
+    cell.appendChild(cancel_button)
+    cancel_button.id = "Cancel_" + ID
+    cancel_button.innerHTML = "取消"
+    cancel_button.style.display = "none"
+    cancel_button.addEventListener("click", function(){
+        controller(this.id)
+    })
+
+}
+
+modify_flag = 0
+
+function controller(object_id){
+    mode = object_id.split("_")[0]
+    id = object_id.split("_")[1]
+
+    confirm_button = document.getElementById("Confirm_" + id)
+    cancel_button = document.getElementById("Cancel_" + id)
+    switch (mode){
+        case "Modify":
+            if(modify_flag == 0){
+                confirm_button.style.display = "inline"
+                cancel_button.style.display = "inline"
+
+            
+                create_modify_form(id)
+                modify_flag = 1
+            }
+            else{
+                alert("不可一次修改多行!")
+            }
+            break
+        case "Delete":
+
+            break
+        case "Confirm":
+            confirm_button.style.display = "none"
+            cancel_button.style.display = "none"
+
+            modify_flag = 0
+            modify_form.submit()
+            break
+        case "Cancel":
+            confirm_button.style.display = "none"
+            cancel_button.style.display = "none"
+
+            modify_flag = 0
+            destroy_modify_form(id)
+            break
+        default:
+            alert("controller gone wrong with mode " + mode + " and id " + id)
+            return
+    }
+}
+
+temp_date = undefined
+temp_power = undefined
+temp_environment = undefined
+
+function create_modify_form(id){
+    table_row = document.getElementById("Row_" + id)
+    date_cell = table_row.children[1]
+    power_cell = table_row.children[2]
+    environment_cell = table_row.children[3]
+    confirm_button = document.getElementById("Confirm_" + id)
+
+    modify_form = document.createElement("form")
+    modify_form.action = "https://httpbin.org/post"
+    modify_form.method = "post"
+    modify_form.id = "modify_form"
+    modify_form.style.display = "none"
+    document.body.appendChild(modify_form)
+
+    date_value = date_cell.innerHTML
+    power_value = power_cell.innerHTML
+    environment_value = environment_cell.innerHTML
+
+    temp_date = date_value
+    temp_power = power_value
+    temp_environment = environment_value
+
+    dummy_ID_input = document.createElement("input")
+    dummy_ID_input.setAttribute("form", "modify_form")
+    dummy_ID_input.name = "Serial_no"
+    dummy_ID_input.value = id
+    dummy_ID_input.style.display = "none"
+
+    date_input = document.createElement("input")
+    date_input.setAttribute("form", "modify_form")
+    date_input.type = "date"
+    date_input.id = "date_" + id
+    date_input.name = "Date"
+    date_input.value = date_value
+    date_cell.innerHTML = ""
+    date_cell.appendChild(date_input)
+
+    power_input = document.createElement("input")
+    power_input.setAttribute("form", "modify_form")
+    power_input.type = "number"
+    power_input.id = "power_" + id
+    power_input.name = "KWH"
+    power_input.value = power_value
+    power_cell.innerHTML = ""
+    power_cell.appendChild(power_input)
+
+    environment_input = document.createElement("select")
+    environment_input.setAttribute("form", "modify_form")
+    environment_input.id = "environment_" + id
+    environment_input.name = "Environment"
+    residential_option = document.createElement("option")
+    residential_option.value = "住宅"
+    residential_option.innerHTML = "住宅"
+    commercial_option = document.createElement("option")
+    commercial_option.value = "商用"
+    commercial_option.innerHTML = "商用"
+    environment_input.appendChild(residential_option)
+    environment_input.appendChild(commercial_option)
+    if(environment_value == "住宅"){
+        residential_option.selected = true
+    }
+    else{
+        commercial_option.selected = true
+    }
+    environment_cell.innerHTML = ""
+    environment_cell.appendChild(environment_input)
+
+}
+
+function destroy_modify_form(id){
+    table_row = document.getElementById("Row_" + id)
+    date_cell = table_row.children[1]
+    power_cell = table_row.children[2]
+    environment_cell = table_row.children[3]
+
+    date_cell.removeChild(date_cell.children[0])
+    date_cell.innerHTML = temp_date
+
+    power_cell.removeChild(power_cell.children[0])
+    power_cell.innerHTML = temp_power
+
+    environment_cell.removeChild(environment_cell.children[0])
+    environment_cell.innerHTML = temp_environment
+
+    document.body.removeChild(document.getElementById("modify_form"))
 }
 
 function loadTab(event, mode){
