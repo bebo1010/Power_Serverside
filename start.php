7ccf93e9c0c2d822c09fd93510e49a5dd3df7036
bebo1010 <nick900828@gmail.com>
2023-06-05 09:31:43 +0800

    Add start page

---
+++ start.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            電費試算
+        </title>
+        <link rel="stylesheet" href="./stylesheet/main.css">
+        <script src="./jquery-3.7.0.min.js">
+            
+        </script>
+    </head>
+
+    <body>
+            <table width="300" border="0" align="center" cellpadding="5" cellspacing="0" bgcolor="#F2F2F2">
+                <tr>
+                    <div class="content">
+                        <td colspan="2" align="center" bgcolor="#CCCCCC">電費試算系統</td>
+                </tr>
+                <tr>
+                    <td valign="baseline">
+                        <button onclick="location = 'login_form.php'">
+                            登入
+                        </button>
+                    </td>
+                    <td valign="baseline">
+                        <button onclick="location = 'register_form.php'">
+                            註冊
+                        </button>
+                    </td>
+                </tr>
+            </table>
+    </body>
+</html>
