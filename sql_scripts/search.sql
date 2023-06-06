9e148cbfd639b50f3d75bbdf3255f5d65897a575
bebo1010 <nick900828@gmail.com>
2023-06-06 12:03:01 +0800

    Add order by date so that results make sense

--- sql_scripts/search.sql
+++ sql_scripts/search.sql
@@ -1,3 +1,4 @@
 SELECT `Serial_no`, `Date`, `KWH`, `Use environment`
 FROM `electricity record`
-WHERE `Date` >= :Start_Date AND `Date` <= :End_Date AND `Use environment` = :Environment;
+WHERE `Date` >= :Start_Date AND `Date` <= :End_Date AND `Use environment` = :Environment
+ORDER BY `Date`;
